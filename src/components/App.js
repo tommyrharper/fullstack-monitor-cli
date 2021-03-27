5a42984030bc15b13a760e25a5cfddb27edefb67
tommyrharper <thomasrobertharper@gmail.com>
2021-03-27 19:50:59 +0000

    Chakra is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import "../index.css";
+import { Button } from "@chakra-ui/react";
 
 const App = ({ props }) => {
-  fetch('/apiTest').then((res) => res.json()).then((res) => {
-    console.log(res);
-  }).catch((err) => {
-    console.log('there was an error: ', err);
-  });
-  return <div>Webpack HMR Example</div>;
+  fetch("/apiTest")
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log("there was an error: ", err);
+    });
+  return (
+    <div>
+      <Button>Yo</Button>
+      Hello World
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './components/App';
-import './index.css';
 
 // Opt-in to Webpack hot module replacement
 if (module.hot) module.hot.accept();
 
 /* eslint-disable no-undef */
 ReactDOM.render(
-  <App />,
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
   document.getElementById('app'),
 );
